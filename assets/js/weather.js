67f803cdfc646dc026a5e7ac266c38c386e84bc9
J Burkholder <Jay.S.Burkholder@gmail.com>
2022-06-16 12:34:47 -0400

    checking out how to get cityName as a global variable

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,3 +1,72 @@
+// ====>user input
+// ====>data gets sent to geo api
+// ====>geo api returns data
+// ====>that data gets put into function var=userLocation
+// ====>userLocation gets concatenated into fetch function for weather api
+// ====>weather api returns data
+// ====>data goes into HTML
+
+
+    
+// Submit location info form
+document.getElementById("cityInputField").onclick = function() {
+    var cityName = document.getElementById("userInput").value;
+    console.log(cityName)
+}
+
+
+
+// geolocation API info
+var requestLocationURL = "https://api.myptv.com/geocoding/v1/locations/by-text?"
+var locationKey = "apiKey=ZTUyNDk1MDAxYjYzNGRjNWE4OWM1Njg4ZTg5NjFhNmQ6MzRiZTdkNGItMWJiNC00NWI0LTkxZmEtNThlNzA5NmNiZGEw"
+
+// ===>
+// city name results from input form
+var cityName = function() {
+    document.getElementById("userInput").value;
+};
+
+// ===>
+// encode the string input values from cityName
+    encodeURIComponent(cityName);
+
+// ===>
+// calling location API
+fetch(requestLocationURL + "&" + cityName + "&" + locationKey)
+    .then(function(response) {
+        return response.json();
+    })
+        .then(function (locationData) {
+                console.log(locationData);
+        })
+            .catch(error => {
+                    console.log("an error occurred, please try again")
+            // })
+
+    // pull long/lat id from array inside of locationData
+                .then(function (response) {
+                    response.json().then(function(longLat) {
+                    var id=longLat.results[0].id;
+
+                    var longLatObj = {
+                        // whatever id longitude and latitude are defined as in the JSON data :id
+                    }
+
+    });
+
+
+
+
+// this is the variable where the long/lat coming back from the first API will be stored
+var userLocation = function() {
+    // myptv api needs to convert this info to lat/long
+    // myptv api needs to send lat/long to weatherbit api   
+    // weatherbit api needs to concatenate var userLocation into fetch request
+}
+
+
+
+
 // weather api info
 var weatherKey = "key=a937108d6e62439e97d0d01f06886bd6"
 var requestWeatherURL = "https://api.weatherbit.io/v2.0/current?";
@@ -17,66 +86,6 @@ fetch(requestWeatherURL + weatherKey)
     });
 
 
-// geolocation API info
-var requestLocationURL = "https://api.myptv.com/geocoding/v1/locations/by-text?"
-var locationKey = "apiKey=ZTUyNDk1MDAxYjYzNGRjNWE4OWM1Njg4ZTg5NjFhNmQ6MzRiZTdkNGItMWJiNC00NWI0LTkxZmEtNThlNzA5NmNiZGEw"
-
-// search city name results from input form
-var cityName = function() {
-    document.getElementById("userInput").value;
-}
-
-// encode the string input values in cityName
-    encodeURIComponent(cityName);
-
-// calling location API
-fetch(requestLocationURL + "&" + cityName + "&" + locationKey)
-    .then(function(response) {
-        return response.json();
-    })
-        .then(function (locationData) {
-                console.log(locationData);
-        })
-            .catch(error => {
-                    console.log("an error occurred, please try again")
-            // })
-
-    // pull long/lat id from array inside of locationData
-                // .then(function(longLat) {
-                //     var id=longLat.results[0].id;
-
-                //     var longLat = {
-
-                //     }
-
-    });
-
-
-    
-    // Submit location info form
-    // const submit = document.getElementById('submit');
-    
-    // submitCity.addEventListener('keyup', (e) + {
-
-    // });
-    document.getElementById("cityInputField").onclick = function() {
-        var cityName = document.getElementById("userInput").value;
-        console.log(cityName)
-    }
-
-
-
-
-
-var userLocation = function() {
-    // myptv api needs to convert this info to lat/long
-    // myptv api needs to send lat/long to weatherbit api
-    // weatherbit api needs to concatenate var userLocation into fetch request
-}
-
-
-
-
 
 
 // // putting json data into html
@@ -89,13 +98,3 @@ var userLocation = function() {
 
 
 
-
-// ====>user input
-// ====>data gets sent to geo api
-// ====>geo api returns data
-// ====>that data gets put into function var=userLocation
-// ====>userLocation gets concatenated into fetch function for weather api
-// ====>weather api returns data
-// ====>data goes into HTML
-// ====>
-// ====>
