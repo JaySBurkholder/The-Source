cb392b0267ded9d3371362457c11d07ae7d6789c
J Burkholder <Jay.S.Burkholder@gmail.com>
2022-06-18 22:25:54 -0400

    FINALLY GOT THIS ********** WEATHER TO WORK, no but we made edits to the html weather container, the two fetch requests, console logging the requests, making things async functions, and cleaning up code.

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -15,34 +15,82 @@ document.getElementById("citySubmit").onclick = function () {
     var cityName = document.getElementById("cityInputField").value;
         console.log(cityName)
         cityLocation(cityName)
+        document.getElementById("usercity").innerHTML = cityName;
 };
 
 // don't have an attitude, grab that longitude and latitude!!!
-        var cityLocation = function(cityName) {
+        var cityLocation = async function(cityName) {
             var locationKey = "6d8a186db6fcb3e3e9cf63088cc332f9"
             var requestLocationURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${locationKey}`
-            fetch(requestLocationURL)
+            await fetch(requestLocationURL)
             .then(function(response) {
                 response.json().then(function(locationData) {
-                    // displayWeather(data, city);
-                    console.log(locationData)
-                        .then(function(lonLat) {
-                            var id=lonLat.results[0].id;
-    
-                            var lonLatObj = {
-                                // whatever id longitude and latitude are defined as in the JSON data :id
-                                "lat":id,
-                                "lon":id,
-                            };
-                        })
+                    console.log(lat);
+                    console.log(lon);
+
+                    
+
+                    var lat = locationData[0].lat;
+                    var lon = locationData[0].lon;
+                    
+                    var latlonObj = {
+                        "latId": lat,
+                        "lonId": lon,
+                    }
+                    weatherForecast(latlonObj)
                 })
-            })    
+        })    
     };
 
+var weatherForecast = async function(weather) {
+    console.log(weather)
+    var weatherKey = "a0095fd1fad6e402ed5242dd036ebc5f"
+    var lat = weather.latId
+    var lon = weather.lonId
+    var requestWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}`
+    await fetch(requestWeatherURL)
+    .then(function(forecast) {
+        forecast.json().then(function(forecast) {
+            console.log(forecast)
+        })
+    })
+}    
+
+    
 
 
-    // pull long/lat id from array inside of locationData
-             
+                    // Display current weather and creating elements
+// var displayWeather = function(weather, searchCity) {
+//     currrentWeather.textContent = "";
+//     cityInputSearch.textContent = searchCity;
+
+//     var currentDay = document.createElement("span")
+//     currentDay.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+//     cityInputSearch.appendChild(currentDay);
+
+//     var icons = document.createElement("img")
+//     icons.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+//     cityInputSearch.appendChild(icons);
+
+//     var temperature = document.createElement("span");
+//     temperature.textContent = "Temperature: " + weather.main.temp + " °F";
+//     temperature.classList = "list-group-item"
+
+//     var humid = document.createElement("span");
+//     humid.textContent = "Humidity: " + weather.main.humidity + " %";
+//     humid.classList = "list-group-item"
+
+//     var windSpeed = document.createElement("span");
+//     windSpeed.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+//     windSpeed.classList = "list-group-item"
+//     currrentWeather.appendChild(temperature);
+//     currrentWeather.appendChild(humid);
+//     currrentWeather.appendChild(windSpeed);
+
+//     var lat = weather.coord.lat;
+//     var lon = weather.coord.lon;
+//     uvIndex(lat,lon);
+// };
     
 
 
@@ -59,7 +107,7 @@ document.getElementById("citySubmit").onclick = function () {
 
 // // weather api info
 // var weatherKey = "733fa984a871e6fcf62ed27f8e4dba3f"
-// var requestWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${weatherKey}"
+// var requestWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${weatherKey}`
 // // var userLocation = function()
 // // + '&' + userLocation
 
