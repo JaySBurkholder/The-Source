ec80aa3e6cc0561c6297f2145ee316e6ac3c73d3
Bradley V <bvenant@aol.com>
2022-06-19 20:29:22 -0400

    pull from develop

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -9,24 +9,24 @@
 
     
 // Submit location info form
-document.getElementById("citySubmit").onclick = function () {
-    var cityName = document.getElementById("cityInputField").value;
-    console.log(cityName);
-}
+// document.getElementById("citySubmit").onclick = function () {
+//     var cityName = document.getElementById("cityInputField").value;
+//     console.log(cityName);
+// }
 
 // city name results from input form
-var cityName = function() {
-    document.getElementById("userInput").value;
-};
+// var cityName = function() {
+//     document.getElementById("userInput").value;
+// };
 
     // encode the string input values from cityName
-    encodeURIComponent(cityName);
+    // encodeURIComponent(cityName);
 
 
 
 // geolocation API info
-var requestLocationURL = "https://api.myptv.com/geocoding/v1/locations/by-text?"
-var locationKey = "apiKey=ZTUyNDk1MDAxYjYzNGRjNWE4OWM1Njg4ZTg5NjFhNmQ6MzRiZTdkNGItMWJiNC00NWI0LTkxZmEtNThlNzA5NmNiZGEw"
+// var requestLocationURL = "https://api.myptv.com/geocoding/v1/locations/by-text?"
+// var locationKey = "apiKey=ZTUyNDk1MDAxYjYzNGRjNWE4OWM1Njg4ZTg5NjFhNmQ6MzRiZTdkNGItMWJiNC00NWI0LTkxZmEtNThlNzA5NmNiZGEw"
 
 
 
@@ -100,3 +100,96 @@ var locationKey = "apiKey=ZTUyNDk1MDAxYjYzNGRjNWE4OWM1Njg4ZTg5NjFhNmQ6MzRiZTdkNG
 // //     document.querySelector('#forecast').append(forecast)
 
 // }
+
+var cityForm = document.querySelector("#city-form");
+var cityInput = document.querySelector("#city");
+var currrentWeather = document.querySelector("#current-weather");
+var currrentWeather = document.querySelector("#current-weather");
+var cityInputSearch = document.querySelector("#searched-city");
+var searchList = document.querySelector("#searchList");
+var apiKey = "92fb9e2a80553525912a61e5eeb8ce46";
+
+var cities = [];
+
+// Submit form funtion
+var submitForm = function(event) {
+    event.preventDefault();
+    var city = cityInput.value.trim();
+    if (city) {
+        cityWeather(city);
+        cities.unshift({city});
+        cityInput.value = "";
+    } else {
+        alert("Please enter a city!")
+    };
+    // searchStorage();
+    //  searched(city);
+};
+
+var cityWeather = function(city) {
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    fetch(apiURL)
+    .then(function(response) {
+        response.json().then(function(data) {
+            displayWeather(data, city);
+        });
+    });
+};
+
+// Display current weather and creating elements
+var displayWeather = function(weather, searchCity) {
+    currrentWeather.textContent = "";
+    cityInputSearch.textContent = searchCity;
+
+    var currentDay = document.createElement("span")
+    currentDay.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+    cityInputSearch.appendChild(currentDay);
+
+    var icons = document.createElement("img")
+    icons.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    cityInputSearch.appendChild(icons);
+
+    var temperature = document.createElement("span");
+    temperature.textContent = "Temperature: " + weather.main.temp + " °F";
+    temperature.classList = "list-group-item"
+
+    var humid = document.createElement("span");
+    humid.textContent = "Humidity: " + weather.main.humidity + " %";
+    humid.classList = "list-group-item"
+
+    var windSpeed = document.createElement("span");
+    windSpeed.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+    windSpeed.classList = "list-group-item"
+
+    currrentWeather.appendChild(temperature);
+
+    currrentWeather.appendChild(humid);
+
+    currrentWeather.appendChild(windSpeed);
+
+    var lat = weather.coord.lat;
+    var lon = weather.coord.lon;
+    uvIndex(lat,lon);
+};
+
+// Latitude and Longitute
+var uvIndex = function(lat, lon) {
+    var futureURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+    fetch(futureURL)
+    .then(function(response) {
+        response.json().then(function(data) {
+            // uvIndexDisplay(data)
+        });
+    });
+};
+
+var searchHandler = function(event) {
+    var city = event.target.getAttribute("data-city")
+    if (city) {
+        cityWeather(city);
+        fiveDay(city);
+    }
+};
+
+cityForm.addEventListener("submit", submitForm);
+searchList.addEventListener("click", searchHandler);
